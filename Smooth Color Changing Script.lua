c491a290952399a0bc4df5fb3a9e168100f13459
vivaanchauhan1969 <141391553+vivaanchauhan1969@users.noreply.github.com>
2024-08-06 01:02:43 -0500

    Update Smooth Color Changing Script.lua

--- Smooth Color Changing Script.lua
+++ Smooth Color Changing Script.lua
@@ -52,3 +52,58 @@ while true do
 	end
 end)
 end
+local block = script.Parent
+db = false
+
+function onTouch()
+	if db == false then
+		db = true
+		for i = 1, 20 do
+			block.Transparency = i/20
+			wait(0.05)
+		end
+		block.CanCollide = false
+		wait(2)
+		block.CanCollide = true
+		block.Transparency = 0
+		db = false
+	end
+end
+
+block.Touched:connect(onTouch)
+		if a == 1 then
+			flow[#flow + 1] = Instance.new("Part")
+			local p = flow[#flow]
+			p.formFactor = form
+			p.Size = siz
+			p.Anchored = true
+			p.CanCollide = false
+			p.TopSurface = 0
+			p.BottomSurface = 0
+			if #flow - 1 > 0 then
+				local pr = flow[#flow - 1]
+				p.Position = torso.Position - torso.Velocity/ndist
+				CFC(p, pr)
+			else
+				p.CFrame = CFrame.new(torso.Position - torso.Velocity/ndist, torso.CFrame.lookVector)
+			end
+			p.BrickColor = BrickColor.new("Cyan")
+			p.Transparency = 1
+			p.Parent = torso
+			local marm = Instance.new("BlockMesh")
+			marm.Scale = Vector3.new(1.9, 0.9, 1.725)
+			marm.Parent = p
+			local amplitude
+			local frequency
+			amplitude = pi
+			desiredAngle = amplitude
+			RightShoulder.MaxVelocity = 0.4
+			LeftShoulder.MaxVelocity = 0.4
+			RightHip.MaxVelocity = pi/10
+			LeftHip.MaxVelocity = pi/10
+			RightShoulder.DesiredAngle = desiredAngle
+			LeftShoulder.DesiredAngle = -desiredAngle
+			RightHip.DesiredAngle = 0
+			LeftHip.DesiredAngle = 0
+		end
+	end
